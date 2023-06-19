735622b93561cffff71500629d3c64db412d3a76
nownityaSmartgig <nownitya.sharma@smartgig.tech>
2023-06-19 16:31:12 +0530

    Initial Commit

---
+++ app/src/main/java/com/smartgig/tech/ui/activities/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.smartgig.tech.ui.activities.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.smartgig.tech.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
