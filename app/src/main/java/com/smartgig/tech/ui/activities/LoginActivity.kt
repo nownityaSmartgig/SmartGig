8f1b6ce495342d96661df892e9a1df357c13889d
vishalSmartgig <vishal.mishra@smartgig.tech>
2023-06-26 14:56:15 +0530

    fragment_apply_leave.xml

--- app/src/main/java/com/smartgig/tech/ui/activities/LoginActivity.kt
+++ app/src/main/java/com/smartgig/tech/ui/activities/LoginActivity.kt
@@ -1,4 +1,4 @@
-package com.smartgig.tech.ui.activities.login
+package com.smartgig.tech.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,6 +7,5 @@ import com.smartgig.tech.R
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
     }
 }
